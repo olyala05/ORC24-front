165e4f3161dddfb27a1a1e5c796701959571da8b
Olya Yusupova <162722757+olyala05@users.noreply.github.com>
2025-02-10 17:41:03 +0300

    düzeltmeler yapıldı

--- static/js/app.js
+++ static/js/app.js
@@ -5,19 +5,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (togglePassword && passwordInput) {
         togglePassword.addEventListener("click", function () {
-            // Şifreyi göster veya gizle
+            // Şifreyi göster/gizle
             if (passwordInput.type === "password") {
                 passwordInput.type = "text";
-                togglePassword.classList.add("active"); // Renk değişimi için
                 togglePassword.innerHTML = '<i class="fa-solid fa-eye-slash"></i>';
             } else {
                 passwordInput.type = "password";
-                togglePassword.classList.remove("active");
                 togglePassword.innerHTML = '<i class="fa-solid fa-eye"></i>';
             }
         });
     }
 });
+
 
 document.addEventListener("DOMContentLoaded", function () {
     const langButtons = document.querySelectorAll(".language-dropdown button");
